544c25c9adcd92580c4183fadcdb8380f8c20231
Madhav <madhavballa231@gmail.com>
2024-01-23 21:51:26 +0530

    form created

---
+++ New folder/script.js
@@ -0,0 +1,13 @@
+//  let namastebtn=document.querySelector('button');
+// namastebtn.addEventListener('click',showmsg);
+
+// function showmsg(){
+//     alert("Namaste World!"); 
+// }
+let namastebtn=document.querySelector("button");
+namastebtn.addEventListener('click',inputMsg);
+ 
+function inputMsg(){
+    let name=prompt('enter name of students');
+    namastebtnbtn.textcontent='Roll No. 1:'+  name}
+
